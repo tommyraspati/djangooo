5c192ab27a8b49f4e3b589a6ca7c50c4234b940f
raspatimahatma <tommymahatma08@gmail.com>
2022-09-06 21:31:29 +0700

    intial commit

--- proyek1/proyek1/settings.py
+++ proyek1/proyek1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1pcgge62=th&8&n6c(mnxg-co3jy#qw!*a&tn0$0z@h(9^0)y8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'proyek1.urls'
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
